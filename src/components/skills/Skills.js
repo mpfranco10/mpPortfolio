a4d5316d4cc5d810ff097373d631d55a51005ad6
ne-mpfranco <mariapaula.franco@j2.com>
2022-12-05 18:05:55 -0500

    skills

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './Skills.css';
+import jsImage from "../../assets/js.png";
+
+function Skill(props) {
+    return (<div className='skill'>
+        <img src={props.icon} className='skill-image' alt={props.text}/>
+        <p className='skill-text'>{props.text}</p>
+    </div>);
+}
 
 function Skills() {
-    return (<div className='skills' id='skills'>
-        <p className='title'>Skills</p>
-        <div>
+    return (
+        <div className='skills' id='skills'>
+            <p className='title'>Skills</p>
+            <div className='skills-icon-container'>
+                <Skill icon={jsImage} text='Javascript'></Skill>
+            </div>
         </div>
-    </div>);
+    );
 }
 
 export default Skills;
